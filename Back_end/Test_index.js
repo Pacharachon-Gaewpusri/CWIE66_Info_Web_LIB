a97682fdda8dc626bde0bd647f002480aefa85b0
Pacharchon Gaewpusri <pacharachongaewpusri@gmail.com>
2026-06-11 13:47:44 +0700

    backend done fow now

---
+++ Back_end/Test_index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'SupremeAdmin',
+  password: 'Internsh1p_;-;',
+  database: 'intern_day3',
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
