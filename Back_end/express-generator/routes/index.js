64d758a7a1abd312eec8a2e988c7b982ad9bdd0a
Pacharchon Gaewpusri <pacharachongaewpusri@gmail.com>
2026-06-12 14:56:28 +0700

    ...

--- Back_end/express-generator/routes/index.js
+++ Back_end/express-generator/routes/index.js
@@ -7,3 +7,21 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+// const mysql = require('mysql');
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'SupremeAdmin',
+//   password: 'Internsh1p_;-;',
+//   database: 'intern_day3',
+// });
+
+// connection.connect();
+
+// connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+//   if (err) throw err;
+
+//   console.log('The solution is: ', rows[0].solution);
+// });
+
+// connection.end();
